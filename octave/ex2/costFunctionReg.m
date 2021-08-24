e6676bfe4d8e696ee765768bc7e4f2e846a146d5
swarajpande4 <swarajpande4@gmail.com>
2021-08-24 17:35:41 +0530

    Added Logistic Regression Assignment in Octave

--- octave/ex2/costFunctionReg.m
+++ octave/ex2/costFunctionReg.m
@@ -17,10 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+A = X';
+h = sigmoid(X * theta);
+J = ((-1/m) * ( (y' * log(h)) + (1-y') * (log(1 - h)))) + ((lambda/(2 * m)) * sum(theta(2:size(theta,1),1) .^ 2));
+grad = ((1 / m) * (X' * (h - y))) + ((lambda / m) * theta);
+grad(1, 1) = ((1 / m) * (A(1, :) * (h - y)));
 
 % =============================================================
 
